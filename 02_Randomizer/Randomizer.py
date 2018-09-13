86d3ecb1c9f008f0c873a7fb55855957718ef480
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-09-12 22:54:54 -0400

    renamed folder according to Mr. Browns specifications replaced solo work with merged work

--- 02_Randomizer/Randomizer.py
+++ 02_Randomizer/Randomizer.py
@@ -1,27 +1,19 @@
-# Mohammed Jamil
-# SoftDev1 pd6
-# K02 -- NO-body expects the Spanish Inquisition!
-# 2018-09-07
-import random
-KREWES = {
+#Addison Huang, Mohammed Jamil
+#softdev1 pd6
+#K #02: NO-body expects the Spanish Inquisition!
+#2018 09 12
 
-        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
-
-        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
-
-        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
-
+KREWES = {'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Don\'t really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+'m': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
 }
-def namePicker():
-    l = list(KREWES.keys())
-    #print(l)
-    #print(random.choice(l))
-    group=KREWES[random.choice(l)]
-    #print(group)
-    return random.choice(group)
 
-# Tests
-i = 0
-while i < 10:
-    print(namePicker())
-    i+=1
+import random
+def randomizer():
+    randTeam = random.randint(0,3)-1 #random number from 0 to 2
+    team = list(KREWES.keys())[randTeam] #the random team that is picked
+    randMember = random.choice(KREWES[team]) #picks a random member of the team
+    return randMember #returns the random member
+print(randomizer())    
+
+    
