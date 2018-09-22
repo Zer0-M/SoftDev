acf2484517fa44d747aa5226850ed36006859c78
Mohammed S. Jamil <mjamil90@stuy.edu>
2018-09-21 22:04:28 -0400

    one column html table can be filled out

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,13 +1,19 @@
 from flask import Flask,render_template
 app = Flask(__name__) #create instance of class Flask
+import randoccgen
+occdict=randoccgen.generateDict('\\data\\occupations.csv')
+keys=list(occdict.keys())
+values=[]
+for key in keys:
+    values.append(occdict[key])
 
 @app.route("/") #assign fxn to route
 def hello_world():
     return "No hablo queso!"
 
-@app.route("/my_foist_template")
-def temptest():
-    return render_template("template.html",collection=[0,1,1,2,3,5,8])
+@app.route("/occupations")
+def occupationtable():
+    return render_template("tabletemplate.html",head0='Job Class',head1='Percentage',collection=keys)
 
 app.debug = True
 app.run()
