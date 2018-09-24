ca39e942fc0d207fc6cd97a19ac358148cc1b78d
Zer0-M <mjamil90@stuy.edu>
2018-09-24 19:41:07 -0400

    camel cased allvariables and functions to make code more consistent

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -7,20 +7,20 @@
 from flask import Flask,render_template
 app = Flask(__name__) #create instance of class Flask
 import randoccgen #use the python file from assignment 3 as a module in order to use its functions(name changed for easier use)
-occdict=randoccgen.generateDict('data/occupations.csv')#create a dictionary from the occupations csv
+occDict=randoccgen.generateDict('data/occupations.csv')#create a dictionary from the occupations csv
 randOcc=randoccgen.getOccupation()#randomly generate an occupation weighted by percentage
-randOccURL = occdict[randOcc][1]#get the url for associated with randomly selected occupation in the csv
+randOccURL = occDict[randOcc][1]#get the url for associated with randomly selected occupation in the csv
 
 @app.route("/")
-def hello_world():
+def landingPage():
     return'''
     <h1>Welcome</h1>
     <br>
     <a href='/occupations'><h3>See Occupations</h3></a>'''
 
 @app.route("/occupations")
-def occupationtable():
-    return render_template("tabletemplate.html",title="Job Picker",Heading="Randomly Selected Job For The Needy",head0='Job Class',head1='Percentage',head2='URL',collection=occdict,occ=randOcc,url=randOccURL)
+def occTable():
+    return render_template("tabletemplate.html",title="Job Picker",Heading="Randomly Selected Job For The Needy",head0='Job Class',head1='Percentage',head2='URL',collection=occDict,occ=randOcc,url=randOccURL)
 
 app.debug = True
 app.run()
