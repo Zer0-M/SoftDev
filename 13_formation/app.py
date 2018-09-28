c37735e03d9a3198532285254e462c64ff9af4b7
Zer0-M <mjamil90@stuy.edu>
2018-09-28 00:38:41 -0400

    app.py now fulfills assignment requirements

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,18 +1,29 @@
+#Mohammed Jamil
+#SoftDev1 pd6
+#K#13 -- Echo Echo Echo
+#2018-09-23
 from flask import Flask,render_template,request
-app = Flask(__name__) #create instance of class Flask
+app = Flask(__name__)
 
-@app.route("/") #assign fxn to route
+@app.route("/")
 def hello_world():
    print(app)
-   return render_template("forms.html")
+   return render_template("forms.html",
+                          title="Authentication", 
+                          heading="Hello")
 @app.route("/auth")
 def authenticate():
     print(app)
     print(request)
-    print(request.args)
-    #print(request.args[0])
-    #print(request.header)
-    return "Waaa hooo HAAH"
+    print(request.headers)#gets the HTML headers
+    print(request.args)#creates an immutable dictionary with the arguments from a GET request
+    print(request.method)#gets the request method
+    print(request.form)#creates an immutable dicitonary with the arguments from a POST request
+    return render_template("Greeting.html",
+                            title="Greeting",
+                            Greeting="Oh Hai ",
+                            username=request.args['username'],#the username is received from the dictionary request.args created
+                            requestmethod=request.method)#request.method gets the method via which the request received
    
 
 app.debug = True
