a3a3e90299a30f76a10f9febbac0026589d9fa28
Zer0-M <mjamil90@stuy.edu>
2018-10-02 00:19:19 -0400

    completed assignment

--- 14_login/app.py
+++ 14_login/app.py
@@ -8,6 +8,8 @@ K13 -- Echo Echo Echo
 
 from flask import Flask, render_template, request, session, url_for, redirect
 app = Flask(__name__)
+import os
+app.secret_key=os.urandom(32)
 
 user = "JAMIIII"
 pswd = "swordfish"
@@ -16,26 +18,39 @@ pswd = "swordfish"
 def home():
     # prints <Flask 'app'> 
     print(app)
+    if 'username' in session:
+        return redirect(url_for("authenticate"))
     return render_template("auth.html")
 
 # default method is GET when POST isn't specified
 @app.route("/usrfail")
 def wrongusr():
+    if 'username' in session:
+        return redirect(url_for("authenticate"))
     return render_template("auth.html", ERROR = "SHAME ON YOU, WRONG USERNAME")
 @app.route("/pwdfail")
 def wrongpwd():
+    if 'username' in session:
+        return redirect(url_for("authenticate"))
     return render_template("auth.html", ERROR = "SHAME ON YOU, WRONG PASSWORD")
 @app.route("/auth", methods=["GET","POST"])
-def authenticate():
-    print(request.cookies)
+def authenticate(): 
     # return "I like watermelon"
+    if 'username' in session:
+        return render_template("welcome.html", username = session['username'], sub1 = request.method)
     usr = request.form["username"]
     passwd = request.form["password"]
     if usr != user:
         return redirect(url_for("wrongusr"))
     elif usr == user and passwd!=pswd:
         return redirect(url_for("wrongpwd"))
-    return render_template("welcome.html", username = usr, password = passwd, sub1 = request.method)
+    session['username']=usr
+    return render_template("welcome.html", username = usr, sub1 = request.method)
+
+@app.route("/signout")
+def logout():
+    session.pop("username")
+    return render_template("auth.html")
 
 app.debug = True
 app.run()
