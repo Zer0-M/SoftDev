6b4c2fd334ecf166948053a3fbb2860bdd610c80
Zer0-M <mjamil90@stuy.edu>
2018-10-07 16:48:56 -0400

    added function that add rows plus more comments

--- 17_db-nmcrnch/db_builder.py
+++ 17_db-nmcrnch/db_builder.py
@@ -12,16 +12,16 @@ c = db.cursor()               #facilitate db ops
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
 def table_creator(name,filename,field0,field1,field2):
-    create_command="CREATE TABLE {0}({1} TEXT, {2} INTEGER, {3} INTEGER);".format(name,field0,field1,field2)
+    create_command="CREATE TABLE {0}({1} TEXT, {2} INTEGER, {3} INTEGER);".format(name,field0,field1,field2)#This sql command creates a table using the parameters given
     c.execute(create_command)
     with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
+        reader = csv.DictReader(csvfile)#The DictReader method turns each row into dictionray with fieldnames as keys 
         for row in reader:
-            #insert into tbl values
+            #The following command will insert the csv data into the table
             insert = "INSERT INTO {0} VALUES('{1}',{2},{3});".format(name,row[field0],row[field1],row[field2])
             c.execute(insert)
-table_creator('peeps','peeps.csv','name','age','id')
-table_creator('courses','courses.csv','code','mark','id')
+table_creator('peeps','peeps.csv','name','age','id')#this creates the table from the peeps csv
+table_creator('courses','courses.csv','code','mark','id')#this creates the table from the courses csv
 #==========================================================
 
 db.commit() #save changes
--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -7,15 +7,20 @@ DB_FILE="studentdata.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
+def add_row(code,mark,id):
+	row = "INSERT INTO courses VALUES('{0}',{1},{2});".format(code,mark,id)
+	c.execute(row)
+#add_row('lunch',102.5,7)
 get_peeps = "SELECT * FROM peeps;"
 people = c.execute(get_peeps).fetchall()#fetchall is a method for the cursor object that gets all the rows that match the conditional in the select command
 
 create_command = "CREATE TABLE peeps_avg(id INTEGER, average INTEGER);"
 c.execute(create_command)
-#print(people)
+
 for student in people: #people is a tuple of tuples
 	get_grades = "SELECT mark FROM courses WHERE courses.id == {0}".format(student[2]) #student[2] is the id of a student 
 	grades = c.execute(get_grades).fetchall() 
+	#print (grades)
 	total = 0.0
 	num = len(grades) #gets the number of grades for each student
 	for grade in grades:  #grade is a tuple with only the grade for one subject inside
