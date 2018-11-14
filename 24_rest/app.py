d3b2475a73ee55ff3365353c09020803706c8aef
Zer0-M <mjamil90@stuy.edu>
2018-11-14 01:19:40 -0500

    I can now rest(work 24 done)

---
+++ 24_rest/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+import urllib.request as request
+import urllib.parse as parse
+import json
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/") #assign fxn to route
+def root():
+    with request.urlopen("https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo") as nasa:
+        dict=json.load(nasa)
+    imgurl=dict["hdurl"]
+    explain=dict["explanation"]
+    print(imgurl)
+    print(explain)
+    return render_template("template.html",img=imgurl,explanation=explain)
+
+app.debug = True
+app.run()
